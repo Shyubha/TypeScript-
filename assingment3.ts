7286546f03d324074d05f3bc5180c0056d5dbfc3
Shubhankar  Kulkarni <86038837+Shyubha@users.noreply.github.com>
2022-04-11 13:35:31 +0530

    Add files via upload
    commit

---
+++ assingment3.ts
@@ -0,0 +1,7 @@
+let studentTuple= [1, 'Shubhankar', 'Computer', 'PUNE'];
+console.log(studentTuple);
+//accesing tupple data
+console.log("Student ID :" + studentTuple[0]);
+console.log("Student Name: " + studentTuple[1]);
+console.log("Student Department:" + studentTuple[2]);
+console.log("Student Address:" + studentTuple[3]);
